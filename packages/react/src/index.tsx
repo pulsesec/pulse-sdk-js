cb9ae44cbd90444613a62e8fa77d86a8f38f51c0
Daniel <damelikyan@gmail.com>
2024-04-26 20:18:54 -0700

    improve

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,3 +1,34 @@
+"use client";
+import { useEffect } from "react";
+
+type Props = {
+	siteKey: string;
+	origin?: string;
+};
+
+export function PulseScript({ siteKey, origin = "https://cdn.pulsesecurity.org" }: Props) {
+	useEffect(() => {
+		let token: string | undefined;
+		let callback: PulseCallback | undefined;
+		window.onpulse =
+			window.onpulse ||
+			function (arg) {
+				if (typeof arg === "string") {
+					token = arg;
+					callback && callback(token);
+					return;
+				}
+				if (token) {
+					arg(token);
+					return;
+				}
+				callback = arg;
+			};
+	}, []);
+
+	return <script src={`${origin}/script/pulse.js`} data-sitekey={siteKey} async />;
+}
+
 type PulseCallback = (token: string) => void;
 
 declare global {
@@ -5,28 +36,3 @@ declare global {
 		onpulse(cb: PulseCallback): void;
 	}
 }
-
-function preamble() {
-	let token: string | undefined;
-	let callback: PulseCallback | undefined;
-	window.onpulse =
-		window.onpulse ||
-		function (arg) {
-			if (typeof arg === "string") {
-				token = arg;
-				callback && callback(token);
-				return;
-			}
-			if (token) {
-				arg(token);
-				return;
-			}
-			callback = arg;
-		};
-}
-
-if (typeof window !== "undefined") {
-	preamble();
-}
-
-export { PulseScript } from "./PulseScript";
