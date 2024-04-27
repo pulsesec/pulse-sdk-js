f5101c272ae5241b8bf1d7914124bb3ad0800e28
Daniel <damelikyan@gmail.com>
2024-04-26 19:13:49 -0700

    customizable origin

--- packages/react/src/PulseScript.tsx
+++ packages/react/src/PulseScript.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 type Props = {
 	siteKey: string;
+	origin?: string;
 };
 
-export function PulseScript({ siteKey }: Props) {
-	return <script src={`https://cdn.pulsesecurity.org/script/pulse.js`} data-sitekey={siteKey} async />;
+export function PulseScript({ siteKey, origin = "https://cdn.pulsesecurity.org" }: Props) {
+	return <script src={`${origin}/script/pulse.js`} data-sitekey={siteKey} async />;
 }
